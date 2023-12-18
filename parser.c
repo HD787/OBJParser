7606dd9ffde1cb10554e27dd8bcad16327dc60bc
henry <Hd116527@gmail.com>
2023-12-17 20:39:57 -0600

    implemented the reading of line flags, nice

---
+++ parser.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void parse(char* path){
+    FILE* file = fopen(path, "r");
+    if(file == NULL){
+        printf("no file :(");
+        fclose(file);
+    }
+    char buf[100];
+    while (fgets(buf, sizeof(buf), file)) {
+        char temp[10];
+        for(int i = 0; i < 10; ++i){
+            if(buf[i] == ' ' | buf[i] == '#'){
+                break;
+            }
+            temp[i] = buf[i];
+        }
+        printf("%s", temp);
+        for(int j=0; j < sizeof(buf); ++j){ buf[j] = 0; }
+    }
+
+
+}
+
+int main(){
+    parse("example.obj");
+}
