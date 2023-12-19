0428ec375762c16c37aaf8012007c7f0c5ae5712
henry <Hd116527@gmail.com>
2023-12-18 21:12:37 -0600

    added some comments for later directions and a slightly brighter shade of green in my graph

--- functions.h
+++ functions.h
@@ -3,7 +3,9 @@
 vertex makeVertex(char* str){
     int quit = 1;
     int i = 2;
+    // walk through string an parse out floats delimited by spaces, ignore starting character in this case 'v'
+    //might be 3 or 4 floats, 2 spaces in a row triggers termination
     while(quit){
-        ;
+        //consider nested loops for this;
     }
 }
