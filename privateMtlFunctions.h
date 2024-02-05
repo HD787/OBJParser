3801fb350f3b5ccbc0229456c8a6de263bb6575b
henry <Hd116527@gmail.com>
2024-02-04 19:44:39 -0600

    i actually got so much done on this, i think i have full UNTESTED mtl parser here, nice job

--- privateMtlFunctions.h
+++ privateMtlFunctions.h
@@ -47,7 +47,6 @@ void parseFloat(float* val, char* buf){
 
 void parseMtl(object* obj, hashMap* hm){
     int size = size()
-    //malloc in some array the value size * sizeof(material)
     FILE* file = fopen(path, "r");
     if(file == NULL){
         printf("no mtl file :(");
@@ -55,11 +54,12 @@ void parseMtl(object* obj, hashMap* hm){
         return;
     }
     char buf[200];
+    tempMtl* material = NULL;
     while(fgets(buf, sizeof(buf), file)) {
 
         //THIS WONT WORK AS EACH ITERATION ONLY DOES ONE VAL
-        tempMtl* material
-        tempMtl = malloc(sizeof(material)); 
+        
+         
         //
 
 
@@ -70,41 +70,87 @@ void parseMtl(object* obj, hashMap* hm){
             }
             temp[i] = buf[i];
         }
-        if(strcmp(temp, "newmtl") == 0){}
+        if(strcmp(temp, "newmtl") == 0){
+            char* slice = buf+=6;
+            if(tempMtl == NULL){
+                tempMtl = malloc(sizeof(material));
+                tempMtl->name = slice
+            }
+            else{
+                insert(hm, tempMtl->name, tempMtl);
+                free(tempMtl);
+                tempMtl = malloc(sizeof(material));
+                tempMtl->name = slice;
+            }
+
+        }
         if(strcmp(temp, "Ka") == 0){
-            parseThreeFloatArray(obj->Ka, buf);
+            parseThreeFloatArray(tempMtl->Ka, buf);
         }
         if(strcmp(temp, "Kd") == 0){
-           parseThreeFloatArray(obj->Kd, buf);
+           parseThreeFloatArray(tempMtl->Kd, buf);
         }
         if(strcmp(temp, "Ks") == 0){
-            parseThreeFloatArray(obj->Ks, buf);
+            parseThreeFloatArray(tempMtl->Ks, buf);
         }
         if(strcmp(temp, "Ke") == 0){
-            parseThreeFloatArray(obj->Ke, buf);
+            parseThreeFloatArray(tempMtl->Ke, buf);
         }
         if(strcmp(temp, "Kt") == 0){
-            parseThreeFloatArray(obj->Kt, buf);
+            parseThreeFloatArray(tempMtl->Kt, buf);
         }
         if(strcmp(temp, "Ns") == 0){
-            parseFloat(&obj->Ns, buf);
+            parseFloat(&tempMtl->Ns, buf);
         }
         if(strcmp(temp, "Ni") == 0){
-            parseFloat(&obj->Ni, buf);
+            parseFloat(&tempMtl->Ni, buf);
         }
         if(strcmp(temp, "Tf") == 0){
-            parseThreeFloatArray(obj->Tf, buf);
+            parseThreeFloatArray(tempMtl->Tf, buf);
         }
         if(strcmp(temp, "d") == 0){
             char* endptr
             buf+=2;
-            obj->d = strtof(buf, &endptr); 
+            tempMtl->d = strtof(buf, &endptr); 
         }
         if(strcmp(temp, "illum") == 0){
             char* endptr
             buf += 6;
-            obj->d = strtol(buf, &endptr);
+            tempMtl->d = strtol(buf, &endptr);
         }
+        if(strcmp(temp, "map_Kd")){
+            char* slice = buf += 6;
+            tempMtl->map_KdPath = slice;
+        }
+        if(strcmp(temp, "map_Ka")){
+            char* slice = buf += 6;
+            tempMtl->map_KaPath = slice;
+        }
+        if(strcmp(temp, "map_Ks")){
+            char* slice = buf += 6;
+            tempMtl->map_KsPath = slice;
+        }
+        if(strcmp(temp, "map_Ke")){
+            char* slice = buf += 6;
+            tempMtl->map_KePath = slice;
+        }
+        if(strcmp(temp, "map_Kt")){
+            char* slice = buf += 6;
+            tempMtl->map_KtPath = slice;
+        }
+        if(strcmp(temp, "map_Ns")){
+            char* slice = buf += 6;
+            tempMtl->map_NsPath = slice;
+        }
+        if(strcmp(temp, "map_refl")){
+            char* slice = buf += 8;
+            tempMtl->map_reflPath = slice;
+        }
+        if(strcmp(temp, "map_Bump")){
+            char* slice = buf += 8;
+            tempMtl->map_BumpPath = slice;
+        }
+
         memset(buf, 0, sizeof(buf));
         memset(temp, 0, sizeof(temp));
     }
